5df07992efc0624d499745243a7e6ee8ae5f05c4
Anwesh43 <anweshthecool0@gmail.com>
2021-04-25 02:00:37 +0530

    adding constants and libraries

--- rotatetonextpartview/src/main/java/com/example/rotatetonextpartview/RotateToNextPartView.kt
+++ rotatetonextpartview/src/main/java/com/example/rotatetonextpartview/RotateToNextPartView.kt
@@ -1,2 +1,26 @@
 package com.example.rotatetonextpartview
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val colors : Array<Int> = arrayOf(
+    "#f44336",
+    "#9C27B0",
+    "#6200EA",
+    "#00C853",
+    "#795548"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val steps : Int = 3
+val parts : Int = 1 + steps
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val rot : Float = 90f
