e77fd1bad360652e9236e6a0d56b946297d7b313
Anwesh43 <anweshthecool0@gmail.com>
2021-04-25 02:12:40 +0530

    adding Float and Int extension methods

--- rotatetonextpartview/src/main/java/com/example/rotatetonextpartview/RotateToNextPartView.kt
+++ rotatetonextpartview/src/main/java/com/example/rotatetonextpartview/RotateToNextPartView.kt
@@ -23,4 +23,9 @@ val scGap : Float = 0.02f / parts
 val strokeFactor : Float = 90f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
-val rot : Float = 90f
+val rot : Float = 180f
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
