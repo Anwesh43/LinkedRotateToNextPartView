61cf19b8af52f1ebcda7f30a7447b0cb7ac276b4
Anwesh43 <anweshthecool0@gmail.com>
2021-04-25 02:36:32 +0530

    adding RTNPNode class

--- rotatetonextpartview/src/main/java/com/example/rotatetonextpartview/RotateToNextPartView.kt
+++ rotatetonextpartview/src/main/java/com/example/rotatetonextpartview/RotateToNextPartView.kt
@@ -122,4 +122,45 @@ class RotateToNextPartView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class RTNPNode(var i : Int, val state : State = State()) {
+
+        private var next : RTNPNode? = null
+        private var prev : RTNPNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = RTNPNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawRTNPNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : RTNPNode {
+            var curr : RTNPNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
