1f23a6cedec78c878404359aab28d01679308c8d
Anwesh43 <anweshthecool0@gmail.com>
2021-04-25 02:29:05 +0530

    adding RotateToNextPartView class

--- rotatetonextpartview/src/main/java/com/example/rotatetonextpartview/RotateToNextPartView.kt
+++ rotatetonextpartview/src/main/java/com/example/rotatetonextpartview/RotateToNextPartView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawRTNPNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawRotateToNextPart(scale, w, h, paint)
 }
+
+class RotateToNextPartView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
