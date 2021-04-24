4f7393fafd05d7ad35fdf3555d96e4811cec6d43
Anwesh43 <anweshthecool0@gmail.com>
2021-04-25 02:45:59 +0530

    adding fullscreen support

--- app/src/main/java/com/example/linkedrotatetonextpartview/MainActivity.kt
+++ app/src/main/java/com/example/linkedrotatetonextpartview/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.linkedrotatetonextpartview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.rotatetonextpartview.RotateToNextPartView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        RotateToNextPartView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
